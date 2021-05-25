d06ef5e148fccd2787d18f929635c784d225a901
Frankie <78957847+c3scno@users.noreply.github.com>
2021-05-25 22:13:52 +0800

    tic-tac-toe: finished HTML skeleton

--- nobleza-frankie/tic_tac_toe/app.js
+++ nobleza-frankie/tic_tac_toe/app.js
@@ -1,6 +1,28 @@
+//JS for Modal
+//DOM Declarations
+const modal = document.querySelector('#modal');
+const modalContainer = document.querySelector('#modal-container');
+const formGridSize = document.querySelector('#form_grid_size');
+const formFirstMove = document.querySelector('#form_first_move');
+const formOpponentSelection = document.querySelector('#form_opponent_selection');
+const formSubmitBtn = document.querySelector('#form_submit');
+const boardWrapper = document.querySelector('#board_wrapper');
+
+//Button on submit
+formSubmitBtn.addEventListener("click", function(){
+    initializeContent();
+})
+
+//JS for Main
+//DOM Declarations
+
 //Global Variables
-const board = [
-    ["","",""],
-    ["","",""],
-    ["","",""]
-]
+
+//Initialize Main
+const initializeContent = () => {
+
+     // Hide/Unhide
+     modal.classList.add('hide');
+     modalContainer.classList.add('hide');
+     content.classList.remove('hide');
+}
